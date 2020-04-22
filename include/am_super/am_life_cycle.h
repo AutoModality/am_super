0973017106665b79d14238b5f943617fc43aacf4
dan hennage <dan@automodality.com>
2020-04-22 15:09:15 -0700

    changed tabs to spaces

--- include/am_super/am_life_cycle.h
+++ include/am_super/am_life_cycle.h
@@ -13,95 +13,98 @@ namespace am
 
 enum class LifeCycleState : std::uint8_t
 {
-	INVALID		        = brain_box_msgs::LifeCycleState::STATE_INVALID,
-	UNCONFIGURED		= brain_box_msgs::LifeCycleState::STATE_UNCONFIGURED,
-	INACTIVE			= brain_box_msgs::LifeCycleState::STATE_INACTIVE,
-	ACTIVE				= brain_box_msgs::LifeCycleState::STATE_ACTIVE,
-	FINALIZED			= brain_box_msgs::LifeCycleState::STATE_FINALIZED,
-	CONFIGURING			= brain_box_msgs::LifeCycleState::STATE_CONFIGURING,
-	CLEANING_UP			= brain_box_msgs::LifeCycleState::STATE_CLEANING_UP,
-	SHUTTING_DOWN		= brain_box_msgs::LifeCycleState::STATE_SHUTTING_DOWN,
-	ACTIVATING			= brain_box_msgs::LifeCycleState::STATE_ACTIVATING,
-	DEACTIVATING		= brain_box_msgs::LifeCycleState::STATE_DEACTIVATING,
-	ERROR_PROCESSING	= brain_box_msgs::LifeCycleState::STATE_ERROR_PROCESSING,
-  	LAST_STATE 			= brain_box_msgs::LifeCycleState::STATE_LAST
+    INVALID = brain_box_msgs::LifeCycleState::STATE_INVALID,
+    UNCONFIGURED = brain_box_msgs::LifeCycleState::STATE_UNCONFIGURED,
+    INACTIVE = brain_box_msgs::LifeCycleState::STATE_INACTIVE,
+    ACTIVE = brain_box_msgs::LifeCycleState::STATE_ACTIVE,
+    FINALIZED = brain_box_msgs::LifeCycleState::STATE_FINALIZED,
+    CONFIGURING = brain_box_msgs::LifeCycleState::STATE_CONFIGURING,
+    CLEANING_UP = brain_box_msgs::LifeCycleState::STATE_CLEANING_UP,
+    SHUTTING_DOWN = brain_box_msgs::LifeCycleState::STATE_SHUTTING_DOWN,
+    ACTIVATING = brain_box_msgs::LifeCycleState::STATE_ACTIVATING,
+    DEACTIVATING = brain_box_msgs::LifeCycleState::STATE_DEACTIVATING,
+    ERROR_PROCESSING = brain_box_msgs::LifeCycleState::STATE_ERROR_PROCESSING,
+    LAST_STATE = brain_box_msgs::LifeCycleState::STATE_LAST
 };
 
 enum class LifeCycleStatus : std::uint8_t
 {
-	OK		    = brain_box_msgs::LifeCycleState::STATUS_OK,
-	WARN		= brain_box_msgs::LifeCycleState::STATUS_WARN,
-	ERROR		= brain_box_msgs::LifeCycleState::STATUS_ERROR,
-  	LAST_STATUS = brain_box_msgs::LifeCycleState::STATUS_LAST
+    OK = brain_box_msgs::LifeCycleState::STATUS_OK,
+    WARN = brain_box_msgs::LifeCycleState::STATUS_WARN,
+    ERROR = brain_box_msgs::LifeCycleState::STATUS_ERROR,
+    LAST_STATUS = brain_box_msgs::LifeCycleState::STATUS_LAST
 };
 
 class AMLifeCycle
 {
 protected:
-	LifeCycleState state_;
-	LifeCycleStatus status_;
+    LifeCycleState state_;
+    LifeCycleStatus status_;
 
-	ros::NodeHandle nh_;
-	ros::Timer heartbeat_timer_;
-	ros::Publisher state_pub_;
+    ros::NodeHandle nh_;
+    ros::Timer heartbeat_timer_;
+    ros::Publisher state_pub_;
 
-	/**
-	* @brief Default constructor
-	*/
-	AMLifeCycle();
+    /**
+     * @brief Default constructor
+     */
+    AMLifeCycle();
 
-	/**
-	* @brief Virtual destructor
-	*/
-	virtual ~AMLifeCycle();
+    /**
+     * @brief Virtual destructor
+     */
+    virtual ~AMLifeCycle();
 
-	virtual void configure();
-	virtual void cleanup();
-	virtual void activate();
-	virtual void deactivate();
-	virtual void shutdown();
-	virtual void destroy();
+    virtual void configure();
+    virtual void cleanup();
+    virtual void activate();
+    virtual void deactivate();
+    virtual void shutdown();
+    virtual void destroy();
 
-	/**
-	* @brief Function to be defined by the user.
-	*        Called at the end of transition from UNCONFIGURED to INACTIVE.
-	*/
-	virtual void onConfigure();
+    /**
+     * @brief Function to be defined by the user.
+     *        Called at the end of transition from UNCONFIGURED to INACTIVE.
+     */
+    virtual void onConfigure();
 
-	/**
-	* @brief Function to be defined by the user.
-	*        Called at the end of transition from INACTIVE to UNCONFIGURED.
-	*/
-	virtual void onCleanUp();
+    /**
+     * @brief Function to be defined by the user.
+     *        Called at the end of transition from INACTIVE to UNCONFIGURED.
+     */
+    virtual void onCleanUp();
 
-	/**
-	* @brief Function to be defined by the user.
-	*        Called at the end of transition from INACTIVE to ACTIVE.
-	*/
-	virtual void onActivate();
+    /**
+     * @brief Function to be defined by the user.
+     *        Called at the end of transition from INACTIVE to ACTIVE.
+     */
+    virtual void onActivate();
 
-	/**
-	* @brief Function to be defined by the user.
-	*        Called at the end of transition from ACTIVE to INACTIVE.
-	*/
-	virtual void onDeactivate();
+    /**
+     * @brief Function to be defined by the user.
+     *        Called at the end of transition from ACTIVE to INACTIVE.
+     */
+    virtual void onDeactivate();
 
-	/**
-	* @brief Function to be defined by the user.
-	*        Called at the end of transition from INACTIVE to FINALIZED.
-	*/
-	virtual void onShutdown();
+    /**
+     * @brief Function to be defined by the user.
+     *        Called at the end of transition from INACTIVE to FINALIZED.
+     */
+    virtual void onShutdown();
 
-	/**
-	* @brief Function to be defined by the user.
-	*        Called after an error and may transition to UNCONFIGURED or FINALIZED.
-	*/
-	virtual void onError();
+    /**
+     * @brief Function to be defined by the user.
+     *        Called after an error and may transition to UNCONFIGURED or FINALIZED.
+     */
+    virtual void onError();
 
-	virtual void heartbeatCB(const ros::TimerEvent& event);
+    virtual void heartbeatCB(const ros::TimerEvent &event);
 
-}; // class AMLifeCycle
+};
+// class AMLifeCycle
 
-}; // namespace am
+}
+;
+// namespace am
 
 #endif
--- src/am_super/am_life_cycle.cpp
+++ src/am_super/am_life_cycle.cpp
@@ -4,13 +4,15 @@
 namespace am
 {
 
-
-AMLifeCycle::AMLifeCycle()  : nh_("~")
+AMLifeCycle::AMLifeCycle() :
+        nh_("~")
 {
-	state_ = LifeCycleState::UNCONFIGURED;
-	status_ = LifeCycleStatus::OK;
-	state_pub_ = nh_.advertise<brain_box_msgs::LifeCycleState>("/node_state",100);
-	heartbeat_timer_ = nh_.createTimer(ros::Duration(1.0), &AMLifeCycle::heartbeatCB, this);
+    state_ = LifeCycleState::UNCONFIGURED;
+    status_ = LifeCycleStatus::OK;
+    state_pub_ = nh_.advertise<brain_box_msgs::LifeCycleState>("/node_state",
+            100);
+    heartbeat_timer_ = nh_.createTimer(ros::Duration(1.0),
+            &AMLifeCycle::heartbeatCB, this);
 }
 
 AMLifeCycle::~AMLifeCycle()
@@ -65,17 +67,17 @@ void AMLifeCycle::onError()
 {
 }
 
-void AMLifeCycle::heartbeatCB(const ros::TimerEvent& event)
+void AMLifeCycle::heartbeatCB(const ros::TimerEvent &event)
 {
-	brain_box_msgs::LifeCycleState msg;
-	msg.node_name = ros::this_node::getName();
-	msg.process_id = 0;
-	msg.state = (uint8_t)state_;
-	msg.status = (uint8_t)status_;
-	msg.subsystem = "";
-	msg.value = "";
-	state_pub_.publish(msg);
+    brain_box_msgs::LifeCycleState msg;
+    msg.node_name = ros::this_node::getName();
+    msg.process_id = 0;
+    msg.state = (uint8_t) state_;
+    msg.status = (uint8_t) status_;
+    msg.subsystem = "";
+    msg.value = "";
+    state_pub_.publish(msg);
 }
 
-
-};
+}
+;

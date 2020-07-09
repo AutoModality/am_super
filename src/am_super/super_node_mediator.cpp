74ac360a43d5c0c5d3a23d83b1293876c12ab0f4
Aaron Roller <aaron.roller@automodality.com>
2020-07-09 16:43:21 -0700

    style: variable names with underscores PL-629/node-mediator

--- src/am_super/super_node_mediator.cpp
+++ src/am_super/super_node_mediator.cpp
@@ -56,10 +56,10 @@ bool SuperNodeMediator::checkActivateState(SuperNodeMediator::SuperNodeInfo& nr)
 pair<bool,map<string,string>> SuperNodeMediator::allManifestedNodesCheck(
   Supervisor supervisor, std::function<bool(SuperNodeMediator::SuperNodeInfo&)> check)
 {
-  map<string,string> failedNodes;
+  map<string,string> failed_nodes;
 
   bool success = true;
-  std::string errorMessage;
+  std::string error_message;
   for (pair<string,SuperNodeInfo> nodePair: supervisor.nodes)
   {
     SuperNodeInfo node = get<1>(nodePair);
@@ -68,25 +68,25 @@ pair<bool,map<string,string>> SuperNodeMediator::allManifestedNodesCheck(
     {
       if(!node.online)
       {
-        errorMessage = "check failed: node not online: " + node.name;
+        error_message = "check failed: node not online: " + node.name;
       }
       else if(!check(node))
       {
           string node_state =to_string((int)node.state);//string(AMLifeCycle::stateToString(node.state));
-          errorMessage= "check failed: node in wrong state (" + node_state + "): " + node.name;
+          error_message= "check failed: node in wrong state (" + node_state + "): " + node.name;
       }
       else if(node.status == LifeCycleStatus::ERROR)
       {
-        errorMessage = "check failed: node status is ERROR: " + node.name;
+        error_message = "check failed: node status is ERROR: " + node.name;
       }
-      if(!errorMessage.empty())
+      if(!error_message.empty())
       {
         success = false;
-        failedNodes.insert(pair<string,string>(node.name,errorMessage));
+        failed_nodes.insert(pair<string,string>(node.name,error_message));
       }
     }//else not manifested so ignore
   }//for each node
-  return pair(success,failedNodes);
+  return pair(success,failed_nodes);
 }
 
 }

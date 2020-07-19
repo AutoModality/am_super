e9779aec909dd8ad63bb8a2aa7d75c912017bfbf
Dillon-Roller <DillonRoller.18@gmail.com>
2020-07-19 14:35:31 -0500

    feat: successfully ensured 3 heartbeats received

--- rostest/super_node_status.cpp
+++ rostest/super_node_status.cpp
@@ -1,30 +1,47 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-#include <gtest/gtest.h>  // googletest header file
+//#include <gtest/gtest.h>  // googletest header file
+#include <brain_box_msgs/VxState.h> //msg for status
 
-void callback(const std_msgs::String::ConstPtr& msg)
+int ARMED_count = 3;
+
+void callback(const brain_box_msgs::VxState& msg)
 {
-  ROS_INFO("I heard: [%s]", msg->data.c_str());
-}
-
-TEST(TestNode, testSuperStatus)
-{
-  ros::NodeHandle n;
-
-  ros::Subscriber sub = n.subscribe("/super/status", 1000, callback);
-  ros::Rate loop_rate(1); //1 Hz
-
-  int count = 3;
-
-  while(count--) {
-    loop_rate.sleep();
-    ros::spinOnce();
+  if(msg.state == brain_box_msgs::VxState::ARMED) 
+  {
+    ROS_INFO_STREAM("heartbeat received..");
+    ARMED_count--;
+  }
+  else 
+  {
+    ROS_INFO_STREAM("heartbeat not received, retrying...");
   }
 }
 
+/*
+TEST(Test, test) 
+{
+  ASSERT_TRUE(true);
+}
+*/
+
 int main(int argc, char **argv)
 {
-  ::testing::InitGoogleTest(&argc, argv);
+  //::testing::InitGoogleTest(&argc, argv);
   ros::init(argc, argv, "subscriber");
-  return RUN_ALL_TESTS();
+  ros::NodeHandle n;
+
+  ros::Subscriber sub = n.subscribe("/vstate/summary", 1, callback);
+  ros::Rate loop_rate(1); //1 Hz
+
+  ROS_INFO_STREAM("Checking for heartbeat until 3 received (Ctrl-C to cancel)..\n");
+  while(ARMED_count && ros::ok()) 
+  {
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+  
+  ROS_INFO_STREAM("System status: ARMED");
+
+  return 0;
 }

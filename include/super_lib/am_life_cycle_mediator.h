cb33b3cd9ad397b1310b65030c5a9ddd516ac368
dillon@dillon <dillon@automodality.com>
2020-08-01 08:30:08 -0500

    docs: function headers for AMLifeCycleMediator

--- include/super_lib/am_life_cycle_mediator.h
+++ include/super_lib/am_life_cycle_mediator.h
@@ -2,22 +2,42 @@
 #define AM_LIFE_CYCLE_MEDIATOR_H_
 
 #include <super_lib/am_life_cycle_types.h>
+using namespace am;
 
 /** Stateless methods providing function without coupling to ROS or any 
  * systems providing testable code.
  */
-using namespace am;
+
 class AMLifeCycleMediator
 {
 
 public:
+    /**
+     * 
+     */
     struct LifeCycleInfo
     {
         LifeCycleStatus status;
     };
-    
+    /**
+     * @brief Sets the current LifeCycleStatus in the mediator
+     * 
+     * @param status    status that we want to set
+     * @param info      mediator enum holding information about LifeCycle
+     * 
+     * @returns true    status is valid and mediator info.status was updated
+     * @returns false   status is invalid and mediator info.status was not updated
+     */ 
     bool setStatus(const LifeCycleStatus& status, LifeCycleInfo& info);
+
+    /**
+     * @brief Gets the current LifeCycleStatus in the mediator
+     * 
+     * @param info mediator enum holding information about LifeCycle
+     * 
+     * @returns info.state - the current LifeCycleStatus in the mediator
+     */ 
     LifeCycleStatus getStatus(const LifeCycleInfo& info);
 };
 
-#endif
+#endif // AM_LIFE_CYCLE_MEDIATOR_H_

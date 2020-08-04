f0c8a6cb74e565b2075f4b13c9252d00328d8aa1
dillon@dillon <dillon@automodality.com>
2020-08-03 22:19:35 -0500

    test: updated tests to give better output on error

--- test/am_life_cycle_mediator_unit_tests.cpp
+++ test/am_life_cycle_mediator_unit_tests.cpp
@@ -7,29 +7,30 @@ using namespace std;
 
 
 
-void ASSERT_LIFE_CYCLE_STATUS(LifeCycleStatus expected_status,bool expected_success)
+void EXPECT_LIFE_CYCLE_STATUS(LifeCycleStatus expected_status,bool expected_success)
 {
   AMLifeCycleMediator mediator;
   AMLifeCycleMediator::LifeCycleInfo info;
   bool success = mediator.setStatus(expected_status, info);
   LifeCycleStatus actual = mediator.getStatus(info);
-  EXPECT_EQ(actual, expected_status);
+  EXPECT_EQ(actual, expected_status) << mediator.statusToString(expected_status) << " not equal to " 
+    << mediator.statusToString(actual);
   EXPECT_EQ(success,expected_success);
 }
 
 TEST(LifeCycleMediator, getAndSetStatus_OK)
 {
-  ASSERT_LIFE_CYCLE_STATUS(LifeCycleStatus::OK, true);
+  EXPECT_LIFE_CYCLE_STATUS(LifeCycleStatus::OK, true);
 }
 
 TEST(LifeCycleMediator, getAndSetStatus_WARN)
 {
-  ASSERT_LIFE_CYCLE_STATUS(LifeCycleStatus::WARN, true);
+  EXPECT_LIFE_CYCLE_STATUS(LifeCycleStatus::WARN, true);
 }
 
 TEST(LifeCycleMediator, getAndSetStatus_ERROR)
 {
-  ASSERT_LIFE_CYCLE_STATUS(LifeCycleStatus::ERROR, true);
+  EXPECT_LIFE_CYCLE_STATUS(LifeCycleStatus::ERROR, true);
 }
 
 TEST(LifeCycleMediator, getAndSetStatus_ignores_LAST_STATUS)
@@ -47,13 +48,14 @@ TEST(LifeCycleMediator, getAndSetStatus_ignores_LAST_STATUS)
   EXPECT_EQ(LifeCycleStatus::OK, final_status); //expect no change in status
 }
 
-void ASSERT_LIFE_CYCLE_STATE(LifeCycleState expected_state,bool expected_success)
+void EXPECT_LIFE_CYCLE_STATE(LifeCycleState expected_state,bool expected_success)
 {
   AMLifeCycleMediator mediator;
   AMLifeCycleMediator::LifeCycleInfo info;
   bool success = mediator.setState(expected_state, info);
   LifeCycleState actual = mediator.getState(info);
-  EXPECT_EQ(actual, expected_state);
+  EXPECT_EQ(actual, expected_state) << mediator.stateToString(expected_state) << " not equal to " 
+    << mediator.stateToString(actual);
   EXPECT_EQ(success,expected_success);
 }
 
@@ -63,13 +65,13 @@ TEST(LifeCycleMediator, getAndSetState_all_except_ERROR_PROCESSING)
 
   for (int i = 0; i < all.size() - 1; i++) //exclude ERROR_PROCESSING
   {
-    ASSERT_LIFE_CYCLE_STATE(all[i], true);
+    EXPECT_LIFE_CYCLE_STATE(all[i], true);
   }
 }
 
 TEST(LifeCycleMediator, getAndSetState_ERROR_PROCESSING)
 {
-  ASSERT_LIFE_CYCLE_STATE(LifeCycleState::ERROR_PROCESSING, true);
+  EXPECT_LIFE_CYCLE_STATE(LifeCycleState::ERROR_PROCESSING, true);
 }
 
 TEST(LifeCycleMediator, commandTestStringConversion)

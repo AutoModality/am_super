61e22c5cc0f389a6ad1cfd0d15e197e09f7072c1
dillon@dillon <dillon@automodality.com>
2020-08-27 13:16:32 -0500

    properly compiles

--- src/am_super/am_super.cpp
+++ src/am_super/am_super.cpp
@@ -465,7 +465,7 @@ private:
    * - all states are UNCONFIGURED or INACTIVE or ACTIVE
    * - all statuses are not error
    */
-  bool allManifestedNodesCheck(std::function<bool(SuperNodeMediator::SuperNodeInfo&)> check)
+  bool allManifestedNodesCheck(std::function<bool(SuperNodeMediator::Supervisor&, SuperNodeMediator::SuperNodeInfo&)> check)
   {
     pair<bool, map<string, string>> result = node_mediator_.allManifestedNodesCheck(supervisor_, check);
     bool success = result.first;

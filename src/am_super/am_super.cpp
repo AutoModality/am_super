f01b31bebc18eacd6604dbc37d926944f268db26
Aaron Roller <aaron.roller@automodality.com>
2020-09-10 16:38:33 -0700

    removing redundant calls to configure and activate already handled by check failures AM-482/super-no-manifest

--- src/am_super/am_super.cpp
+++ src/am_super/am_super.cpp
@@ -544,25 +544,6 @@ private:
     }
     else
     {
-      // send lifecycle updates for selected state transitions
-      switch (supervisor_.system_state)
-      {
-        case SuperState::BOOTING:
-          if(state == SuperState::READY)
-          {
-            ROS_INFO_STREAM("sending CONFIGURE to all nodes");
-            sendLifeCycleCommand(AMLifeCycle::BROADCAST_NODE_NAME, LifeCycleCommand::CONFIGURE);
-          }
-          break;
-        case SuperState::READY:
-          if(state == SuperState::ARMING)
-          {
-            ROS_INFO_STREAM("sending ACTIVATE to all nodes");
-            sendLifeCycleCommand(AMLifeCycle::BROADCAST_NODE_NAME, LifeCycleCommand::ACTIVATE);
-          }
-          break;
-          //all others do not send command
-      }
 
       // persist given state as the new current state
       supervisor_.system_state = state;

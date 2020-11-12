ce7bacca157faa088e27122739485bb5b0ae85f9
dillon@dillon <dillon@automodality.com>
2020-11-11 19:34:02 -0600

    feat: system transitions ABORT -> DISARMING on OP:LANDED

--- src/am_super/super_node_mediator.cpp
+++ src/am_super/super_node_mediator.cpp
@@ -36,7 +36,8 @@ SuperNodeMediator::SuperNodeMediator(const std::string& node_name):
     {SuperState::READY, {SuperState::READY, SuperNodeMediator::checkReadyToArm, LifeCycleCommand::DEACTIVATE}}
   }},
   {SuperState::ABORT, {
-    {SuperState::MANUAL, {SuperState::MANUAL, SuperNodeMediator::checkReadyToArm, LifeCycleCommand::DEACTIVATE, OperatorCommand::MANUAL}}
+    {SuperState::MANUAL, {SuperState::MANUAL, SuperNodeMediator::checkReadyToArm, LifeCycleCommand::DEACTIVATE, OperatorCommand::MANUAL}},
+    {SuperState::DISARMING, {SuperState::DISARMING, SuperNodeMediator::checkArmed, StateTransition::NO_LIFECYCLE_COMMAND, OperatorCommand::LANDED}},
   }},
   {SuperState::MANUAL, {
     {SuperState::DISARMING, {SuperState::DISARMING, SuperNodeMediator::checkNodesActiveOrInactive, StateTransition::NO_LIFECYCLE_COMMAND, OperatorCommand::LANDED}},

b3f47c9c48479e25ab0dc956b34a6d81b7863ce9
dillon@dillon <dillon@automodality.com>
2021-01-07 13:55:15 -0600

    test: update semi_auto_to_manual to use rostest lib

--- rostest/semi_auto_to_manual/semi_auto_to_manual_rostest.cpp
+++ rostest/semi_auto_to_manual/semi_auto_to_manual_rostest.cpp
@@ -1,20 +1,32 @@
-#include "../rostest_transition.h" //FIXME: currently relative path
+#include <am_rostest_lib/am_rostest.h>
 
-using namespace brain_box_msgs;
-
-class SemiAutoToManual : public RostestTransition {};
-
-TEST_F(SemiAutoToManual, testState_SemiAutoToManual)
+class SemiAutoToAuto : public RostestBase, am::AMLifeCycle
 {
-  RostestTransition::sendCommandUntilResponseReceived(OperatorCommand::ARM, RostestTransition::armed);
-  RostestTransition::sendCommandUntilResponseReceived(OperatorCommand::LAUNCH, RostestTransition::in_auto);
-  RostestTransition::sendCommandUntilResponseReceived(OperatorCommand::PAUSE, RostestTransition::semi_auto);
-  RostestTransition::sendCommandUntilResponseReceived(OperatorCommand::MANUAL, RostestTransition::manual);
+protected:
+  SemiAutoToAuto() : RostestBase(ros::this_node::getName()) {}
+};
+
+TEST_F(SemiAutoToAuto, testState_SuccessfulFlight)
+{
+  waitUntil(LifeCycleState::CONFIGURING);
+  waitUntil(LifeCycleState::INACTIVE);
+  waitUntilMissionState(brain_box_msgs::VxState::READY);
+  arm();
+  waitUntil(LifeCycleState::ACTIVE);
+  waitUntilMissionState(brain_box_msgs::VxState::ARMED);
+  launch();
+  waitUntilMissionState(brain_box_msgs::VxState::AUTO);
+  pause();
+  waitUntilMissionState(brain_box_msgs::VxState::SEMI_AUTO);
+  resume();
+  waitUntilMissionState(brain_box_msgs::VxState::AUTO);
+  //the test launch file timeout acts as an assertion if any wait is blocked and unfinishing
 }
 
 int main(int argc, char** argv)
 {
   ::testing::InitGoogleTest(&argc, argv);
   ros::init(argc, argv, ros::this_node::getName());
+
   return RUN_ALL_TESTS();
 }

877a06ceca9c726d957eae2b8e9b4fae0c40d6e1
dillon@dillon <dillon@automodality.com>
2021-01-19 19:07:15 -0600

    unit test now passing

--- src/am_super/super_node_mediator.cpp
+++ src/am_super/super_node_mediator.cpp
@@ -223,11 +223,11 @@ pair<bool, map<string, string>> SuperNodeMediator::allManifestedNodesCheck(
 {
   map<string, string> failed_nodes;
   bool success = true;
-  std::string error_message;
+  
   for (pair<string, SuperNodeInfo> nodePair : supervisor.nodes)
   {
     SuperNodeInfo node = nodePair.second;
-    
+    std::string error_message;
     // only check manifested nodes, ignore others
     if (node.manifested)
     {

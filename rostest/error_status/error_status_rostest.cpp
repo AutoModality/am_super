4b265034eeeb50dcb42309549c18c25d7814f103
dillon@dillon <dillon@automodality.com>
2021-01-20 19:17:27 -0600

    docs: error_status_rostest

--- rostest/error_status/error_status_rostest.cpp
+++ rostest/error_status/error_status_rostest.cpp
@@ -12,7 +12,10 @@ TEST_F(LifeCycleErrorTest, testStatus_Error)
   waitUntil(LifeCycleState::INACTIVE);
   waitUntilMissionState(brain_box_msgs::VxState::READY);
 
+  //simulate an error in LifeCycle
   am::AMLifeCycle::error();
+
+  //LifeCycle State and Status should go error for this node
   waitUntilStatus(LifeCycleStatus::ERROR);
   waitUntil(LifeCycleState::ERROR_PROCESSING);
 }

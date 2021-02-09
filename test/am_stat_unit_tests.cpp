b1d5f7ea3c620d62b6894beb11bfeffb83c9d675
dillon@dillon <dillon@automodality.com>
2021-02-09 16:04:43 -0600

    test: unit test get and set

--- test/am_stat_unit_tests.cpp
+++ test/am_stat_unit_tests.cpp
@@ -12,8 +12,59 @@ protected:
   const string long_name = "Stat 1";
   const uint32_t max_warn = 5;
   const uint32_t max_error = 10;
+
   AMStatTest(): stat_("s1", "Stat 1", 5, 10) {}
 };
+
+TEST_F(AMStatTest, getShortName_constructed)
+{
+  ASSERT_EQ(short_name, stat_.getShortName());
+  //get methods will ensure constructor is working
+}
+
+TEST_F(AMStatTest, getLongName_constructed)
+{
+  ASSERT_EQ(long_name, stat_.getLongName());
+}
+
+TEST_F(AMStatTest, getMaxWarn_constructed)
+{
+  ASSERT_EQ(max_warn, stat_.getMaxWarn());
+}
+
+TEST_F(AMStatTest, getMaxError_constructed)
+{
+  ASSERT_EQ(max_error, stat_.getMaxError());
+}
+
+TEST_F(AMStatTest, getCount_constructed)
+{
+  //0 is initial value
+  ASSERT_EQ(0, stat_.getCount());
+}
+
+TEST_F(AMStatTest, setMaxWarn)
+{
+  int val = max_warn + 1;
+  stat_.setMaxWarn(val);
+  ASSERT_EQ(val, stat_.getMaxWarn());
+}
+
+TEST_F(AMStatTest, setMaxError)
+{
+  int val = max_error + 1;
+  stat_.setMaxError(val);
+  ASSERT_EQ(val, stat_.getMaxError());
+}
+
+TEST_F(AMStatTest, setWarnError)
+{
+  int warn = max_warn + 1;
+  int error = max_error + 1;
+  stat_.setWarnError(warn, error);
+  ASSERT_EQ(warn, stat_.getMaxWarn());
+  ASSERT_EQ(error, stat_.getMaxError());
+}
 
 TEST_F(AMStatTest, compoundStatus)
 {
@@ -55,16 +106,6 @@ TEST_F(AMStatTest, reset)
   stat_ = 3;
   stat_.reset();
   ASSERT_EQ(stat_.getCount(), 3);
-}
-
-TEST_F(AMStatTest, getShortName)
-{
-  ASSERT_EQ(short_name, stat_.getShortName());
-}
-
-TEST_F(AMStatTest, getLongName)
-{
-  ASSERT_EQ(long_name, stat_.getLongName());
 }
 
 TEST_F(AMStatTest, add)

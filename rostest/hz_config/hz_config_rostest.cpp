04b5aedab8509eb6353f0b04cc4a500f0215b498
Aaron Roller <aaron.roller@automodality.com>
2021-08-19 16:04:22 -0700

    test:  default stats configurations are used AM-777/configure_stat

--- rostest/hz_config/hz_config_rostest.cpp
+++ rostest/hz_config/hz_config_rostest.cpp
@@ -13,6 +13,8 @@ protected:
   AMStatReset hz_stats_target_one_hundred_min_error_10_ = AMStatReset("target_one_hundred_min_error_10", "target_one_hundred_min_error_10");
   AMStatReset hz_stats_min_only_ = AMStatReset("min_only", "min_only");
   AMStatReset hz_stats_max_only_ = AMStatReset("max_only", "max_only");
+  AMStat stat_none_ = AMStat("stat_none", "stat_none");
+  AMStat stat_none_with_default_max_ = AMStat("stat_none_with_default_max", "stat_none_with_default_max",15,20);
 
   HzConfigTest() : RostestBase() {
     configureHzStat(hz_stats_target_ten_);
@@ -23,6 +25,8 @@ protected:
     configureHzStat(hz_stats_target_one_hundred_min_error_10_);
     configureHzStat(hz_stats_min_only_);
     configureHzStat(hz_stats_max_only_);
+    configureStat(stat_none_);
+    configureStat(stat_none_with_default_max_);
   }
 
 };
@@ -60,6 +64,20 @@ TEST_F(HzConfigTest, no_target)
 {
   ASSERT_FALSE(hz_stats_no_target_.isValidatingMax());
   ASSERT_FALSE(hz_stats_no_target_.isValidatingMin());
+}
+
+TEST_F(HzConfigTest, stat_none)
+{
+  ASSERT_FALSE(stat_none_.isValidatingMax());
+  ASSERT_FALSE(stat_none_.isValidatingMin());
+}
+TEST_F(HzConfigTest, stat_none_with_default_max)
+{
+  ASSERT_TRUE(stat_none_with_default_max_.isValidatingMax());
+  ASSERT_FALSE(stat_none_with_default_max_.isValidatingMin());
+  //magic numbers provided upon initialization of stat
+  ASSERT_EQ(stat_none_with_default_max_.getMaxWarn(),15);
+  ASSERT_EQ(stat_none_with_default_max_.getMaxError(),20);
 }
 
 TEST_F(HzConfigTest, min_only)
